b2e59413a6d342b0c853509e16ea9ca3e691589f
Sonya <sofpoliakova@gmail.com>
2022-10-10 20:04:50 +0300

    правки

--- js/main.js
+++ js/main.js
@@ -1,22 +1,18 @@
 function getRandomNum(min, max) {
-  const str = NaN;
+  if (typeof min !== 'number' || typeof max !== 'number') {
+    return NaN;
+  }
+
   if (min > max) {
     [min, max] = [max, min];
-  } // проверка на соответствие порядка входных данных
-  if (min === null || max === null) {
-    return str;
-  } // проверка на соответствие входных данных с null
+  }
 
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-console.log(getRandomNum(1, 10));
-console.log(getRandomNum(10, 1));
-console.log(getRandomNum());
 
-const str = 'Hello';
-function valid(s, n) {
-  return s.length === n;
+function check(string, maxLenght) {
+  return string.length <= maxLenght;
 }
 
-console.log(valid(str, 5));
+
