35abc4987201cb2a20be6681910387dfe1ecbbbf
Sonya <sofpoliakova@gmail.com>
2022-10-30 15:36:52 +0300

    правка

--- js/util.js
+++ js/util.js
@@ -14,5 +14,4 @@ function getRandomNum(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-export {checkLenght};
-export {getRandomNum};
+export {checkLenght, getRandomNum};
